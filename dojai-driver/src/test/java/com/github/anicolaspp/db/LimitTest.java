fa92217e92b4eee491fea9dfcd68d3d47561c329
Nicolas A Perez <anicolaspp@gmail.com>
2019-07-28 21:42:02 -0400

    Addint LimitTest

---
+++ dojai-driver/src/test/java/com/github/anicolaspp/db/LimitTest.java
@@ -0,0 +1,37 @@
+package com.github.anicolaspp.db;
+
+import com.github.anicolaspp.ojai.JavaOjaiTesting;
+import lombok.val;
+import net.sf.jsqlparser.expression.LongValue;
+import net.sf.jsqlparser.statement.select.Limit;
+import org.junit.Test;
+
+import java.util.Random;
+
+public class LimitTest implements JavaOjaiTesting {
+
+    @Test
+    public void testNullLimit() {
+        Limit nullLimit = null;
+
+        val queryLimit = QueryLimit.limit(nullLimit);
+
+        val emptyQuery = connection().newQuery();
+
+        val queryWithLimit = queryLimit.applyTo(emptyQuery);
+
+        assert queryWithLimit == emptyQuery;
+    }
+
+    @Test
+    public void testRandomLimit() {
+        val limit = new Random().nextInt(10);
+
+        Limit theLimit = new Limit();
+        theLimit.setRowCount(new LongValue(limit));
+
+        val query = QueryLimit.limit(theLimit).applyTo(connection().newQuery());
+
+        assert query.asJsonString().equals(connection().newQuery().limit(limit).asJsonString());
+    }
+}
