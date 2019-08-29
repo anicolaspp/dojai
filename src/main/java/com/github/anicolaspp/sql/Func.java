d8b3ef1898fca6c6829f380edcebb91d157348d8
Nicolas A Perez <anicolaspp@gmail.com>
2019-08-29 14:46:31 -0400

    Update Func.java

--- src/main/java/com/github/anicolaspp/sql/Func.java
+++ src/main/java/com/github/anicolaspp/sql/Func.java
@@ -2,12 +2,6 @@ package com.github.anicolaspp.sql;
 
 import java.sql.SQLException;
 
-/**
- * This is an Alias for Function<A, B> but allows us to throw SQLException.
- *
- * @param <A> Input Type
- * @param <B> Output Type
- */
 @FunctionalInterface
 public interface Func<A, B> {
     B apply(A value) throws SQLException;
