618de5491ccb44fd05b95fa6881848a208a219f7
Nicolas A Perez <anicolaspp@gmail.com>
2019-08-29 14:25:48 -0400

    No updates for now

--- src/main/java/com/github/anicolaspp/parsers/update/UpdateStatementParser.java
+++ src/main/java/com/github/anicolaspp/parsers/update/UpdateStatementParser.java
@@ -1,14 +1,9 @@
 package com.github.anicolaspp.parsers.update;
 
-import com.github.anicolaspp.db.QueryConditionBuilder;
-import com.github.anicolaspp.db.QueryLimit;
-import com.github.anicolaspp.db.Table;
 import com.github.anicolaspp.parsers.ChainParser;
 import com.github.anicolaspp.parsers.ParserQueryResult;
 import com.github.anicolaspp.parsers.insert.InsertStatementParser;
-import lombok.val;
 import net.sf.jsqlparser.statement.Statement;
-import net.sf.jsqlparser.statement.update.Update;
 import org.ojai.store.Connection;
 
 public class UpdateStatementParser implements ChainParser {
@@ -26,37 +21,42 @@ public class UpdateStatementParser implements ChainParser {
 
     @Override
     public ParserQueryResult getQueryFrom(Statement statement) {
-
-        if (!(statement instanceof Update)) {
-            return ParserQueryResult
-                    .builder()
-                    .successful(false)
-                    .build();
-        }
-
-        val update = (Update) statement;
-        val query = QueryLimit.limit(update.getLimit())
-                .applyTo(connection
-                        .newQuery()
-                        .where(QueryConditionBuilder
-                                .from(update.getWhere())
-                                .buildWith(connection, null)));
-
-        if (update.getColumns() != null && update.getColumns().size() > 0) {
-            for (int i = 0; i < update.getColumns().size(); i++) {
-
-                //TODO: set for different EXPRESION types.
-//                connection.newMutation()
-//                        .set(update.getColumns().get(i), update.getExpressions().get(i))
-
-            }
-        }
-
-        String table = Table.from(update).getName();
-
-        System.out.println("update");
-
-        return null;
+        return ParserQueryResult
+                .builder()
+                .successful(false)
+                .build();
+//
+//
+//        if (!(statement instanceof Update)) {
+//            return ParserQueryResult
+//                    .builder()
+//                    .successful(false)
+//                    .build();
+//        }
+//
+//        val update = (Update) statement;
+//        val query = QueryLimit.limit(update.getLimit())
+//                .applyTo(connection
+//                        .newQuery()
+//                        .where(QueryConditionBuilder
+//                                .from(update.getWhere())
+//                                .buildWith(connection, null)));
+//
+//        if (update.getColumns() != null && update.getColumns().size() > 0) {
+//            for (int i = 0; i < update.getColumns().size(); i++) {
+//
+//                //TODO: set for different EXPRESION types.
+////                connection.newMutation()
+////                        .set(update.getColumns().get(i), update.getExpressions().get(i))
+//
+//            }
+//        }
+//
+//        String table = Table.from(update).getName();
+//
+//        System.out.println("update");
+//
+//        return null;
     }
 }
 
